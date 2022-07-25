0709ce6c97ae5578623fcec83a3c577903bfd0ec
Wojciech Prusaczyk <wojciech.prusaczyk1@gmail.com>
2022-07-25 13:07:27 +0200

    pawnclass

---
+++ src/pawn/Pawn.java
@@ -0,0 +1,20 @@
+public class Pawn extends Coordinates {
+
+    private String color;
+    private boolean isCrowned;
+    public Pawn(int x, int y, boolean isCrowned, String color) {
+        super(x, y);
+        this.isCrowned = isCrowned;
+        this.color= color;
+
+    }
+
+
+
+
+
+
+
+
+
+}
